ffb2f0862174b5120270d68a6e4cd43f0f2d130e
Unknown <axel.murat@laposte.net>
2018-06-22 16:57:20 +0200

    gitignore + rewrite of biopi.js
    Un fonctionnement + objet

--- biopi.js
+++ biopi.js
@@ -3,12 +3,53 @@ const Discord = require("discord.js");
 
 const client = new Discord.Client();
 
-const token = "Enter_Key_Here";
+const config = require("./config.json");
 
-const StarboundServer = "starcookies.verygames.net:21025";
-modsRequired = {EnhancedStorage:"http://community.playstarbound.com/resources/enhanced-storage.2450/",
-FrackingUniverse: "http://community.playstarbound.com/resources/frackinuniverse.2920/"};
-const prefix = "!";
+const commandList = {
+  starbound: "st",
+  help:"help",
+
+  afficherCommande:function(){
+    message.channel.send('')
+  }
+}
+
+var StarboundServer = {
+  adress: "starcookies.verygames.net:21025",
+  status: "OFF",
+  mods: {
+    EnhancedStorage:"http://community.playstarbound.com/resources/enhanced-storage.2450/",
+    FrackingUniverse: "http://community.playstarbound.com/resources/frackinuniverse.2920/"
+  },
+  commands:["info","add","remove"],
+  afficherInfo: function(message){
+    modsInfo = '';
+    for (var key in this.mods) {
+     modsInfo += ('\n\n-['+ key + ']('+this.mods[key]+')');
+    }
+    message.channel.send('```Markdown\nAdresse : '+ this.adress +'\n'+ 'status : ' +this.status +'\n'+ 'mods requis : ' +modsInfo +'```')
+  },
+  executeCommand: function(message){
+    args = message.content.split(" ").slice(1);
+    console.log(args[0]);
+    switch(args[0]){
+      case 'info': this.afficherInfo(message);
+        break;
+      case 'add': mods[args[1]] = args[2];
+        break;
+      default: message.channel.send('Erreur, paramètre "'+args[0]+'" inconnu !');
+    }
+  } 
+}
+
+function executeCommand(message){
+  if(message.content.startsWith(config.prefix + commandList.starbound)){
+    StarboundServer.executeCommand(message)
+  }
+  else if(message.content.startsWith(config.prefix + commandList.help)){
+    message.channel.send('En construction');
+  }
+}
 
 
 client.on('ready', () => {
@@ -17,37 +58,9 @@ client.on('ready', () => {
 
 });
 
-
-
 client.on('message', message => {
-  if (message.content.startsWith(prefix + "st")) {
-    let args = message.content.split(" ").slice(1);
-    switch(args[0]){
-      case 'info':
-      info = "";
-      for (var key in modsRequired) {
-        info += ('\n\n-'+ key + ', '+modsRequired[key]);
-      }
-      message.channel.send('Serveur Biopi : ' + StarboundServer + '\n\n Mods requis : '+info);
-      break;
-      case 'add':
-      if(args.length == 3){
-        name = args[1];
-        link = args[2];
-        modsRequired[name] = link;
-      }
-      else{
-        message.channel.send('Erreur de format : la syntaxe est !st add <nom> <lien>');
-      }
-      break;
-      case 'remove':
-      name = args[1];
-      delete modsRequired[name];
-      break;
-      default:
-      message.channel.send('Argument inconnu');
-    }
-  }
+  executeCommand(message);
 });
+
 // Log our bot in
-client.login(token);
+client.login(config.token);
